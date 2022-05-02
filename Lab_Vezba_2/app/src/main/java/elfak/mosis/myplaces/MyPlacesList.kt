dc17cc2e2f5fdaed942938a417593ae1e6aa3c09
DANILO-LAPTOP\Danilo <danilopotpara@elfak.rs>
2022-05-02 16:24:54 +0200

    Uradjena druga lab vezba, Danilo Potpara 17315 i Nemanja Stojanovic 17459

---
+++ Lab_Vezba_2/app/src/main/java/elfak/mosis/myplaces/MyPlacesList.kt
@@ -0,0 +1,70 @@
+package elfak.mosis.myplaces
+
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.snackbar.Snackbar
+import elfak.mosis.myplaces.databinding.ActivityMyPlacesListBinding
+
+
+class MyPlacesList : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMyPlacesListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMyPlacesListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        var places = arrayOf("Tvrdjava","Cair","Park Svetog Save","Trg Kralja Milana")
+        val pregled:ListView = findViewById(elfak.mosis.myplaces.R.id.my_places_list)
+        val arrayAdapter:ArrayAdapter<String> = ArrayAdapter(this
+            ,android.R.layout.simple_list_item_1
+            ,places)
+        pregled.adapter = arrayAdapter;
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(elfak.mosis.myplaces.R.menu.menu_my_places_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            elfak.mosis.myplaces.R.id.show_map_item -> Toast.makeText(this,"Show Map!", Toast.LENGTH_SHORT).show()
+            elfak.mosis.myplaces.R.id.new_place_item -> Toast.makeText(this,"New Place!", Toast.LENGTH_SHORT).show()
+            elfak.mosis.myplaces.R.id.about_item -> {
+                val i: Intent = Intent(this,About::class.java)
+                startActivity(i)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()
+    }
+}
